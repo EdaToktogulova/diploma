bd4e14b61ac015dfd4067adf1becc9b63ca0cad4
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-04-06 15:22:31 +0600

    Add font-size

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,6 +12,7 @@ function Header() {
          <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500&display=swap" rel="stylesheet"/>
          </head>
          <h1>Perfumery</h1>
+         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet"/>
          <p>This is an internet shop</p>
       </header>
    );
