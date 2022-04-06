f1cc429b9f4fcd84c19e3c08e7be2009a383c3ac
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-04-06 15:07:32 +0600

    Add font-family

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,9 @@ function Header() {
 
    return (
       <header className="Header" style={style}>
+         <head>
+         <link href="https://fonts.googleapis.com/css2?family=Rubik+Moonrocks&display=swap" rel="stylesheet"/>
+         </head>
          <h1>Hello world</h1>
          <p>This is an internet shop</p>
       </header>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,6 +3,9 @@ import "./Nav.css";
 function Nav() {
    return (
       <nav className="Nav">
+         <head>
+         <link href="https://fonts.googleapis.com/css2?family=Rubik+Bubbles&family=Rubik+Wet+Paint&display=swap" rel="stylesheet"/>
+         </head>
          <div>Perfume Shop</div>
          <ul>
             <li><a href="/">Home</a></li>
