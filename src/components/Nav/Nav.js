477e6e0743d54bf3c7f3523e55a98f51781bec35
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-04-13 14:35:25 +0600

    added some news

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,5 @@
 import Logo from "../ui/Logo/Logo";
 import "./Nav.css";
-import NavItem from "./NavItem/NavItem"
 
 function Nav() {
    return (
@@ -9,13 +8,6 @@ function Nav() {
          <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet"/>
          </head>
          <Logo/>
-         <ul>
-            <NavItem url="/" >Home</NavItem>
-            <NavItem url="/categories">Categories</NavItem>
-            <NavItem url="/about" >About</NavItem>
-            <NavItem url="/products">Products</NavItem>
-            <NavItem url="/contacts">Contacts</NavItem>
-         </ul>
       </nav>
    );
 }
