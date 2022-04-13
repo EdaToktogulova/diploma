6efcacb4366bab231a50eecc10f8c3bfcc2a3f11
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-04-13 15:02:22 +0600

    Add new font to ul

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,6 +9,7 @@ function Nav() {
          <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet"/>
          </head>
          <Logo/>
+         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet"/>
          <ul>
             <NavItem url="/">Home</NavItem>
             <NavItem url="/categories">Categories</NavItem>
