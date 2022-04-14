804b342c5a436e87900e6e3d6820d62ecd1b7a18
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-04-14 14:12:12 +0600

    Add new color

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,9 +5,7 @@ import NavItem from "./NavItem/NavItem";
 function Nav() {
    return (
       <nav className="Nav">
-         <head>
          <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet"/>
-         </head>
          <Logo/>
          <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet"/>
          <ul>
