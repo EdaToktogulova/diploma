ede16976f4b0c65fc11a48dee36788a4d88cfaaf
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-04-21 23:18:45 +0600

    Added new jpeg file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 function Header({ image, title, children }) {
    return (
       <header className="Header">
-         <img src = {image} alt="photo" />
+         <img src={image} />
          <section>
          <h1>{title}</h1>
          <p>{children}</p>
