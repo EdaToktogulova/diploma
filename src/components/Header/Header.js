2e77958c545869dae6b12c8f9391f42935523950
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-04-21 18:23:37 +0600

    Added new jpeg file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,9 @@ function Header({ image, title, children }) {
       <header className="Header">
          <img src={image} />
          <section>
-         <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet"/>
+         {/* <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet"/> */}
          <h1>{title}</h1>
-         <link href="https://fonts.googleapis.com/css2?family=Lemonada:wght@300&display=swap" rel="stylesheet"/>
+         {/* <link href="https://fonts.googleapis.com/css2?family=Lemonada:wght@300&display=swap" rel="stylesheet"/> */}
          <p>{children}</p>
          </section>
       </header>
