21104206dcc2c7741152176be4c549adbcc78094
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-04-21 23:14:14 +0600

    Cleanup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 function Header({ image, title, children }) {
    return (
       <header className="Header">
-         <input type="image" img src = {image} alt="photo" />
+         <img src = {image} alt="photo" />
          <section>
          <h1>{title}</h1>
          <p>{children}</p>
