fcad28d7781f1411f469b26b46a189d409cd6d3f
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-30 09:50:23 +0600

    Add Header component to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,15 @@
 import Header from "../components/Header/Header";
-import headerImage from "../assets/home.jpeg";
-import CategoryList from "../components/CategoryList/CategoryList";
-import { getCategories } from "../data/categories";
+// import headerImage from "../assets/home1.jpeg";
 
 function Home() {
    return (
       <>
          <Header
-            title="Welcome to our coffee shop!"
-            image={headerImage}>
-            A coffee bean is a seed of the Coffea plant and the source for coffee. It is the pip inside the red or purple fruit often referred to as a cherry. Just like ordinary cherries, the coffee fruit is also a so-called stone fruit. Even though the coffee beans are not technically beans, they are referred to as such because of their resemblance to true beans.
+            title="Welcome to our perfume shop!"
+            image={headerImage}>Perfume types reflect the concentration of aromatic compounds in a solvent, which in fine fragrance is typically ethanol. Various sources differ considerably in the definitions of perfume types. 
          </Header>
 
-         <CategoryList categories={getCategories()} />
+         
       </>
    );
 }
