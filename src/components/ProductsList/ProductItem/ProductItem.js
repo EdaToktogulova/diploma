6340f5a25ba3095f0d52219d46612a25b3666afc
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-30 10:12:09 +0600

    Add ProductItem component

--- src/components/ProductsList/ProductItem/ProductItem.js
+++ src/components/ProductsList/ProductItem/ProductItem.js
@@ -5,8 +5,8 @@ function ProductItem({ product }) {
    return (
       <div className={classes.ProductItem}>
          <img src={product.image} alt={product.title} />
-         <Link to={"/products/" + product.productId}>{product.title}</Link>
          <div className={classes.price}>${product.price}</div>
+         <Link to={"/products/" + product.productId}>{product.title}</Link>
       </div>
    );
 }
