810bbe6916ee05c285c6784bd9a3c6a4e2755acf
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-30 10:11:29 +0600

    Add Footer.js pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,37 @@
-import "./Footer.css";
+import React from "react";
+import './Footer.css'
+import {FaFacebookSquare, FaInstagramSquare, FaTwitterSquare} from 'react-icons/fa'
 
-function Footer() {
+const Footer = () => {
    return (
-      <footer className="Footer">
+      <div className="footer ">
          <div className="container">
-            Perfume Shop 2022
+            <div className="col">
+               <h3>Address</h3>
+               <p>Adres: Kalverstraat 71, 1012 NZ Amsterdam</p>
+            </div>
+            <div className="col">
+               <h3>About us</h3>
+               <p>A multi-brand store for rare cool contemporary perfume houses. Plus mix your own bespoke perfume at our lab.</p>
+            </div>
+            <div className="col">
+               <h3>Our sponsors</h3>
+               <p>Elon Muck</p>
+               <p>Cristiano Ronaldo</p>
+               <p></p>
+            </div>
+            <div>
+               <h3>Let's Connect</h3>
+               <div className="social">
+                  <FaFacebookSquare className="icon" />
+                  <FaInstagramSquare className="icon" />
+                  <FaTwitterSquare className="icon" />
+               </div>
+            </div>
          </div>
-      </footer>
-   );
+      </div>
+   )
 }
+
 
 export default Footer;
