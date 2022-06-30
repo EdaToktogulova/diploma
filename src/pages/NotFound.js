cba6892ead4300d107e3e3e2771f18c062901fe7
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-30 09:51:54 +0600

    Add Header component to NoFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,1 +1,17 @@
+import React from "react";
+import headerImage from "../assets/categories.jpeg";
+// import Header from "../components/Header/Header";
 
+function NotFound() {
+   return (
+      <>
+         <Header
+            title="Page not found"
+            image={headerImage}>
+            Maybe the page you are looking for was moved or deleted.
+         </Header>
+      </>
+   );
+}
+
+export default NotFound;
