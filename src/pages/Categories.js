908cd3e55a58ae0ec99db8715877f834c2c6be2b
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-30 09:30:45 +0600

    Add import Header in Categories.js

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -11,7 +11,6 @@ function Categories() {
             image={headerImage}>
             The fruits; cherries or berries, most commonly contain two stones with their flat sides together. A small percentage of cherries contain a single seed, instead of the usual two. This is called a "peaberry".
          </Header>
-
          <CategoryList categories={getCategories()} />
       </>
    );
