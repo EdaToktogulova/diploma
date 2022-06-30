14e7eb64f9bc5b3b821587b9ae2477c38cf7373b
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-30 10:14:04 +0600

    Add icon to Logo component

--- src/components/ui/Logo/Logo.js
+++ src/components/ui/Logo/Logo.js
@@ -1,8 +1,10 @@
 import "./Logo.css";
+import { GiWaterFlask } from "react-icons/gi";
 
 function Logo() {
    return (
-      <div className="Logo">Perfume Shop</div>
+      <div className="Logo"><div><GiWaterFlask size={34} style={{ marginleft: '4px' }} /></div></div>
+      
    );
 }
 
