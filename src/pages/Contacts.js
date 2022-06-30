c5b54f48801f68a29050e13da7ef9913c886bef7
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-30 10:23:26 +0600

    Add new title to Contact.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -9,7 +9,12 @@ function Contacts() {
             image={headerImage}>
                A multi-brand store for rare cool contemporary perfume houses. Plus mix your own bespoke perfume at our lab. Samples. Find us at Covent Garden and online.
          </Header>
-         <div>Address: Kalverstraat 71, 1012 NZ Amsterdam</div>
+         <div>Address: Kalverstraat 71, 1012 NZ Amsterdam,
+         Kalverstraat 71, 1012 NZ Amsterdam
+         </div>
+         <div>Address: Kalverstraat 71, 1012 NZ Amsterdam,
+         Kalverstraat 71, 1012 NZ Amsterdam
+         </div>
          <div>/</div>
          <div>.</div>
       </>
