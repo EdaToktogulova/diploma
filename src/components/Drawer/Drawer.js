491cb646a43125661512220ddd3b3ab340e4f2b2
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-30 10:35:42 +0600

    Add onClick=toggle to Drawer component

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -2,10 +2,11 @@ import Nav from "../Nav/Nav";
 import Logo from "../ui/Logo/Logo";
 import classes from "./Drawer.module.css";
 
+
 function Drawer({ open, toggle }) {
    const classNames = [classes.Drawer];
-   // if (open) {
-   //    classNames.push(classes.open);
+   if (open) {
+      classNames.push(classes.open);
    }
 
    return (
