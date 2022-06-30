652b38a9ba4dac23e89adb9832dbc5d1095bc242
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-30 09:45:38 +0600

    Add image to Contact component

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,5 +1,5 @@
 import Header from "../components/Header/Header";
-// import headerImage from "../assets/contacts.jpeg";
+import headerImage from "../assets/contacts.jpeg";
 
 function Contacts() {
    return (
